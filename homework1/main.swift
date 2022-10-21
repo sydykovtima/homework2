344868a0f256d0d69504ab86f31c9c04a46c7a84
sydykovtima <116348777+sydykovtima@users.noreply.github.com>
2022-10-21 17:35:15 +0600

    Update main.swift

--- homework1/main.swift
+++ homework1/main.swift
@@ -1,11 +1,29 @@
 //
 //  main.swift
 //  homework1
-//
-//  Created by Mac on 21/10/2022.
-//
 
 import Foundation
 
 print("Hello, World!")
 
+func square(square1:Int) {
+    print(square1 * square1)
+}
+square(square1: 2)
+
+func cube(cube1: Int) {
+    print(cube1 * cube1 * cube1)
+}
+cube(cube1: 2)
+func perimetr(perimtr1:Int) {
+    print(perimtr1 * 8)
+}
+perimetr(perimtr1: 3)
+func ploshad(a:Int, b:Int) {
+    print((a+b) * 2)
+}
+ploshad(a: 2, b: 3)
+func krug (s:Double, pr:Double, r:Double){
+    print(s * (pr) * (r * r))
+}
+krug(s: 5, pr: 3.14, r: 4)
